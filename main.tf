ae66d2b4e4447893b93a8627743b8a0946494d34
Daniel Tsoref <tsorefdaniel@gmail.com>
2024-07-29 21:57:02 +0300

    updated

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+#----------------------------------------
+# Home Assignment Moveo
+# 1. VPC with public and private subnets.
+# 2. EC2 Instance in the private subnet with dockerized NGINX. 
+# 3. Dynamic Security Groups for maintainability.
+# 4. ALB for public subnets for load balancing EC2 instances. 
+# 5. NAT Gateway to ensure security and internet access for private subnets without giving them public ip.
+# 
+# Daniel Tsoref --- 29.07.2024
+#----------------------------------------
+
+
+
 terraform {
   required_version = ">= 1.0.0"
 
